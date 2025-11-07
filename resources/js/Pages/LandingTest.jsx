1776d554354de3eb00c9d90dcbb6ca286ad79937
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-11-07 21:36:21 +0700

    upgrade tailwind

--- resources/js/Pages/LandingTest.jsx
+++ resources/js/Pages/LandingTest.jsx
@@ -43,7 +43,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             aria-hidden="true"
                         >
                             <div
-                                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 dark:opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                                className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 dark:opacity-20 sm:left-[calc(50%-30rem)] sm:w-288.75"
                                 style={{
                                     clipPath:
                                         'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -61,7 +61,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <Link
                                     href={route('register')}
-                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-solid focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Daftar Sekarang
                                 </Link>
