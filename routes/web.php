27ab7ac767c99a022fa9ed2eefeb8ea7f2595fb7
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-11-13 11:28:15 +0000

    sync

--- routes/web.php
+++ routes/web.php
@@ -84,11 +84,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/chat/{course_id}', [ChatMessageController::class, 'index']);
-    Route::get('/chat/{course_id}/last', [ChatMessageController::class, 'getLastMessage']);
-    Route::post('/chat/{course_id}', [ChatMessageController::class, 'store']);
 });
+
+Route::get('/chat/{course_id}', [ChatMessageController::class, 'index']);
+Route::get('/chat/{course_id}/last', [ChatMessageController::class, 'getLastMessage']);
+Route::post('/chat/{course_id}', [ChatMessageController::class, 'store']);
+
 
 Route::prefix('course')->group(function () {
     Route::get('/', [CourseController::class, 'index'])->name('course.index');
