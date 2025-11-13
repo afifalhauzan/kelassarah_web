9c4b6ba402acfad74d920c51efc4635a4c7eb699
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-11-14 01:26:40 +0700

    feat: add CourseSeeder to seed course data in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        
+        // Seed courses first (materials might depend on courses)
+        $this->call([
+            CourseSeeder::class,
+        ]);
+        
         Material::factory(10)->create();
-        Course::factory(10)->create();
+        // Course::factory(10)->create(); // Commented out since we're using CourseSeeder
         
     }
 }
