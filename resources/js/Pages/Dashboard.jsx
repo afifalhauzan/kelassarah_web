6fd9088e6ad4336578876257612ebd41fef1ff4c
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-11-13 09:42:12 +0700

    Revert "feat : add dashboard page"
    This reverts commit a4562b7b1996f542e1550fd6e2b706c6e8a803ec.

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,56 +1,13 @@
-import { useEffect, useState } from "react";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, usePage } from "@inertiajs/react";
-import ProgressCard from "@/Components/feature/lms/ProgressCard";
-import CourseSlider from "@/Components/feature/lms/CourseSlider";
-// import route from "ziggy-js";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
 export default function Dashboard() {
-    const { auth } = usePage().props;
-    const [courses, setCourses] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        axios
-            .get(route("courses.index"))
-            .then((response) => {
-                setCourses(response.data);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.error("Gagal ngambil data courses:", error);
-                setIsLoading(false);
-            });
-    }, []);
     return (
         <AuthenticatedLayout>
-            <Head title="Dashboard" />
-
-            <div className="py-4">
-                <div className="max-w-7xl mx-auto px-2">
-                    <div className="overflow-hidden">
-                        <div className="p-6 md:p-8 text-gray-900">
-                            <h1 className="text-2xl font-bold text-gray-800">
-                                Halo {auth.user.name}!
-                            </h1>
-                            <p className="text-gray-500 mt-1">
-                                Selamat datang kembali di beranda
-                            </p>
-
-                            <div className="mt-8">
-                                <ProgressCard
-                                    title="Politik Etis dan kebangkitan kaum terpelajar"
-                                    progress={65}
-                                />
-                            </div>
-
-                            {isLoading ? (
-                                <div className="mt-12 text-center">
-                                    <p>Lagi ngambil data kursus...</p>
-                                </div>
-                            ) : (
-                                <CourseSlider courses={courses} /> 
-                            )}
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-xs sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            You're logged in!
                         </div>
                     </div>
                 </div>
