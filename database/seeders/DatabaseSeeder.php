8d8b33aa118c3c10b68f1a394629bf5f51b6fd59
afifalhauzan <afifalhauzan@student.ub.ac.id>
2025-11-18 08:47:36 +0700

    feat: update CourseSeeder and QuizSeeder with new course materials and quiz questions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CourseSeeder::class,
             QuizSeeder::class,
+            UserSiswaSeeder::class,
         ]);
     }
 }
